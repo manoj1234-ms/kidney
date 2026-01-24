461c5d8cb093ecd0e0c4282cba2317473f472bc3
Manoj Sharma <148334299+manoj1234-ms@users.noreply.github.com>
2026-01-24 14:31:17 +0530

    prediction added

---
+++ src/cnnClassifier/pipeline/prediction.py
@@ -0,0 +1,34 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import os
+
+
+
+class PredictionPipeline:
+    def __init__(self,filename):
+        self.filename =filename
+
+
+    
+    def predict(self):
+        model = load_model(os.path.join("artifacts", "training", "model.h5"))
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size=(224, 224))
+        test_image = image.img_to_array(test_image)
+    
+        # ✅ normalization (CRUCIAL)
+        test_image = test_image / 255.0
+    
+        test_image = np.expand_dims(test_image, axis=0)
+    
+        # ✅ get raw probability
+        prediction_prob = model.predict(test_image)[0][0]
+        print("Raw prediction:", prediction_prob)
+    
+        if prediction_prob > 0.5:
+            prediction = "Tumor"
+        else:
+            prediction = "Normal"
+    
+        return [{"image": prediction}]
